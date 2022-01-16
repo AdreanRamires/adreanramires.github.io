13c366484bd89af358c0243c46630c1bbe708097
Adrean Ramires <adreanramires@gmail.com>
2022-01-16 14:17:34 +0100

    profile menu

--- src/main/Header/ProfileMenu.js
+++ src/main/Header/ProfileMenu.js
@@ -12,6 +12,38 @@ const ProfileMenu = () => {
       <img src={image} alt="" className={styles["profile-image"]} />
       <p className={styles.username}>Adrean Ramires</p>
       <i className="fas fa-chevron-down"></i>
+
+      <nav className={styles["nav-menu"]}>
+        <ul className={styles["nav-ul"]}>
+          <li className={styles["nav-li"]}>
+            <i className={`${"far fa-envelope"} ${styles.icon}`}></i>
+            <a href="" className={styles.link}>
+              messages
+            </a>
+          </li>
+
+          <li className={styles["nav-li"]}>
+            <i className={`fas fa-users ${styles.icon}`}></i>
+            <a href="" className={styles.link}>
+              friends
+            </a>
+          </li>
+
+          <li className={styles["nav-li"]}>
+            <i className={`far fa-address-card ${styles.icon}`}></i>
+            <a href="" className={styles.link}>
+              profile
+            </a>
+          </li>
+
+          <li className={styles["nav-li"]}>
+            <i className={`fas fa-sign-out-alt ${styles.icon}`}></i>
+            <a href="" className={styles.link}>
+              sign out
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
